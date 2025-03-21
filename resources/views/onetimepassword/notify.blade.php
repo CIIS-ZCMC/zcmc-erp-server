a4de2b5a6b84f313e4cdebee0856c520b21b8d87
= <=>
2025-03-21 11:17:45 +0800

    update resource for template for email

---
+++ resources/views/onetimepassword/notify.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>One-Time Password</title>
+</head>
+<body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #f4f4f4; color: #333;">
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f9f9f9;">
+        <div style="max-width: 600px; margin: auto; background-color: #ffffff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+            <!-- Header Section -->
+            <img src="https://zcmc.online/zcmc_banner_logo.png" alt="Telemedicine Logo" style="width: 100%; max-height: 250px; object-fit: cover; display: block; margin-bottom: 20px;">
+
+            <!-- Content Section -->
+            <div style="padding: 20px; background-color: #fff;">
+                <p style="text-align: center; font-size: 16px; font-weight: 600; margin: 0 0 20px; color: #333;">Your One-Time Password (OTP)</p>
+                
+                <p style="text-align: center; margin: 30px 0; font-size: 38px; font-weight: bold; color: #0a380a; letter-spacing:10px;">123456</p>
+                
+                <!-- Content Section -->
+                <div style="padding: 20px; text-align: left;">
+                    <p style="font-size: 14px;">If you did not request this OTP, please ignore this email or contact our support team immediately.</p>
+                    <p style="margin: 0 0 10px; font-size: 14px; color: #333;">
+                        <span style="color: #0d271e;"><strong>For your security:</strong></span> Never share this OTP with anyone. ZCMC will never ask for your OTP via phone or email.
+                    </p>
+                    <p>Best regards,</p>
+                    <p style="color: #0d271e; font-size: 14px;"><strong>ZCMC Issu Team</strong></p>
+                </div>
+            </div>
+            
+            <!-- Footer Section -->
+            <div style="text-align: center; padding: 20px 0; font-size: 12px; color: #666; border-top: 1px solid #eaeaea;">
+                <p style="margin: 0; color:  #0d271e;">This email was sent from the <strong>ZCMC APMS Platform</strong>.</p>
+                <p style="margin: 5px 0;">For support, contact us at <a href="mailto:itzcmc.s1918@gmail.com" style="color: #0d271e; text-decoration: none;">itzcmc.s1918@gmail.com</a></p>
+                <p style="margin: 5px 0;">&copy; 2023 ZCMC-APMS. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('one-time-password', function () {
+    return view('onetimepassword.notify');
+});
