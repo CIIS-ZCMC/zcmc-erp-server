42f5ee2d3380848ac7860a526ab9c9347c05aada
mustaham <mustahammicah@gmail.com>
2025-03-21 14:38:40 +0800

    feat(setup): added some base/parent tables and pivot tables, models and controllers

---
+++ app/Http/Controllers/FunctionObjectiveController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FunctionObjective;
+use Illuminate\Http\Request;
+
+class FunctionObjectiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FunctionObjective $functionObjective)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FunctionObjective $functionObjective)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FunctionObjective $functionObjective)
+    {
+        //
+    }
+}
