03f9f4e62631a9fb55979d17b5b62127fb02c6bc
mustaham <mustahammicah@gmail.com>
2025-04-02 10:45:42 +0800

    feat(model): added new model for Application Timeline

---
+++ app/Models/ApplicationTimeline.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ApplicationTimeline extends Model
+{
+    protected $fillable = [
+        'aop_application_id',
+        'ppmp_application_id',
+        'user_id',
+        'current_area_id',
+        'next_area_id',
+        'status',
+        'remarks',
+        'date_created',
+        'date_approved',
+        'date_returned'
+    ];
+
+    use SoftDeletes;
+
+    public function aopApplication(): BelongsTo
+    {
+        return $this->belongsTo(AopApplication::class);
+    }
+
+    public function ppmpApplication(): BelongsTo
+    {
+        return $this->belongsTo(PpmpApplication::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function currentArea(): BelongsTo
+    {
+        return $this->belongsTo(AssignedArea::class, 'current_area_id');
+    }
+
+    public function nextArea(): BelongsTo
+    {
+        return $this->belongsTo(AssignedArea::class, 'next_area_id');
+    }
+}
