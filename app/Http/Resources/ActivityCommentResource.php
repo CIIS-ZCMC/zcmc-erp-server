7400c5ce6ae05ae881a17d4e977d72c2fb446dc2
mustaham <mustahammicah@gmail.com>
2025-04-08 08:59:10 +0800

    feat: updated feature for the ActivityComment

--- app/Http/Resources/ActivityCommentResource.php
+++ app/Http/Resources/ActivityCommentResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\ActivityResource;
+use App\Http\Resources\UserResource;
 
 class ActivityCommentResource extends JsonResource
 {
@@ -14,6 +16,13 @@ class ActivityCommentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'activity' => new ActivityResource($this->activity),
+            'user' => new UserResource($this->user),
+            'comment' => $this->comment,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
