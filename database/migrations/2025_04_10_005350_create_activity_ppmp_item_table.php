9060b3f81b7d77efc2465713af9f1bd22671bd87
kimdee <dhoracekay@gmail.com>
2025-04-10 09:10:44 +0800

    update migration of activity_ppmpitem and ppmpitem_comment

--- database/migrations/2025_04_10_005350_create_activity_ppmp_item_table.php
+++ database/migrations/2025_04_10_005350_create_activity_ppmp_item_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('activity_ppmp_item', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('activity_id');
+            $table->foreign('activity_id')->references('id')->on('activities');
+            $table->unsignedBigInteger('ppmp_item_id');
+            $table->foreign('ppmp_item_id')->references('id')->on('ppmp_items');
+            $table->string('remarks')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2025_04_10_005432_create_ppmp_item_comments_table.php
+++ database/migrations/2025_04_10_005432_create_ppmp_item_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('ppmp_item_comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('ppmp_item_id');
+            $table->foreign('ppmp_item_id')->references('id')->on('ppmp_items');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('comment');
             $table->timestamps();
         });
     }
