76f7b14e34aae7b980f66150b4e4781a29a7b21d
kimdee <dhoracekay@gmail.com>
2025-04-11 10:44:42 +0800

    fix merge

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,6 @@ class DatabaseSeeder extends Seeder
             TypeOfFunctionSeeder::class,
             ProcurementModeSeeder::class,
             AopApplicationSeeder::class,
-                // ApplicationTimelineSeeder::class,     
-            AopApplicationSeeder::class,
             ApplicationTimelineSeeder::class,
             ItemUnitSeeder::class,
             ObjectiveSeeder::class,
