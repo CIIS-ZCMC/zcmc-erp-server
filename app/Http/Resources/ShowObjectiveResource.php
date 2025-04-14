5e4c93fee44f68ec6b82eb14038ae522e0cda767
mustaham <mustahammicah@gmail.com>
2025-04-14 11:33:57 +0800

    fix: naming issue

--- app/Http/Resources/ShowObjectiveResource.php
+++ app/Http/Resources/ShowObjectiveResource.php
@@ -15,7 +15,7 @@ class ShowObjectiveResource extends JsonResource
     public function toArray(Request $request): array {
         return [
             'id' => $this->id,
-            'activity_name' => $this->activity_name,
+            'activity_name' => $this->name,
             'start_month' => $this->start_month,
             'end_month' => $this->end_month,
             'target' => $this->target ? [
