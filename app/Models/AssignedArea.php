239f83a2f490be88a5c19b79831e7e9d0d711a2e
mustaham <mustahammicah@gmail.com>
2025-04-15 09:50:59 +0800

    feat: new resource and functions to get area details

--- app/Models/AssignedArea.php
+++ app/Models/AssignedArea.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
-use App\Models\TransactionLog;  
+use App\Models\TransactionLog;
 use App\Models\User;
 use App\Models\Division;
 use App\Models\Department;
 use App\Models\Section;
 use App\Models\Unit;
+use App\Http\Resources\AssignAreaDivisionResource;
+use App\Http\Resources\AssignAreaDepartmentResource;
+use App\Http\Resources\AssignAreaSectionResource;
+use App\Http\Resources\AssignAreaUnitResource;
 
 /**
  * AssignedArea Model
@@ -85,6 +89,35 @@ class AssignedArea extends Pivot
     public function unit()
     {
         return $this->belongsTo(Unit::class);
+    }
+
+    public function findDetails()
+    {
+        if ($this->division_id !== null) {
+            return [
+                'details' => new AssignAreaDivisionResource($this->division),
+                'sector' => 'Division'
+            ];
+        }
+
+        if ($this->department_id !== null) {
+            return [
+                'details' => new AssignAreaDepartmentResource($this->department),
+                'sector' => 'Department'
+            ];
+        }
+
+        if ($this->section_id !== null) {
+            return [
+                'details' => new AssignAreaSectionResource($this->section),
+                'sector' => 'Section'
+            ];
+        }
+
+        return [
+            'details' => new AssignAreaUnitResource($this->unit),
+            'sector' => 'Unit'
+        ];
     }
 
     /**
