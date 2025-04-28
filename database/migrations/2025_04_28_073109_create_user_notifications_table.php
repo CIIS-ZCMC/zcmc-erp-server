5f1ed3c20576ddf7d28f1b02bacc947c39ab9fd4
mustaham <mustahammicah@gmail.com>
2025-04-28 15:37:41 +0800

    feat: setup for the notif module

---
+++ database/migrations/2025_04_28_073109_create_user_notifications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('seen')->default(false);
+            $table->unsignedBigInteger('notification_id')->unsigned();
+            $table->foreign('notification_id')->references('id')->on('notifications')->onDelete('cascade');
+            $table->unsignedBigInteger('employee_profile_id')->unsigned();
+            $table->foreign('employee_profile_id')->references('id')->on('employee_profiles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_notifications');
+    }
+};
