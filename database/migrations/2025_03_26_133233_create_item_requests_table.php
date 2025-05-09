42dd36480e061ebadba1ff0095bc047d73a1aa3d
kimdee <dhoracekay@gmail.com>
2025-05-09 09:40:15 +0800

    unfinish ticket #63

--- database/migrations/2025_03_26_133233_create_item_requests_table.php
+++ database/migrations/2025_03_26_133233_create_item_requests_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,8 @@ return new class extends Migration
     {
         Schema::create('item_requests', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('activity_id')->nullable();
+            $table->foreign('activity_id')->references('id')->on('activities');
             $table->string('name');
             $table->string('code')->nullable();
             $table->text('image')->nullable();
@@ -40,7 +41,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('item_requests', function(Blueprint $table){
+        Schema::table('item_requests', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
 
