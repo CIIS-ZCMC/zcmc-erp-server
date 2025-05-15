692752e226e1679ab0c258b83140bff33aaffb86
mustaham <mustahammicah@gmail.com>
2025-05-15 10:23:43 +0800

    feat: send mail updated for enhanced functionality

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -9,16 +9,39 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendMail extends Mailable
+class SendMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     /**
+     * The email subject.
+     */
+    public string $subject;
+    
+    /**
+     * The email content/message.
+     */
+    public string $emailContent;
+    
+    /**
+     * The URL for the button.
+     */
+    public ?string $actionUrl;
+    
+    /**
+     * The button text.
+     */
+    public ?string $actionText;
+
+    /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(string $subject, string $emailContent, ?string $actionUrl = null, ?string $actionText = null)
     {
-        //
+        $this->subject = $subject;
+        $this->emailContent = $emailContent;
+        $this->actionUrl = $actionUrl;
+        $this->actionText = $actionText;
     }
 
     /**
@@ -27,7 +50,7 @@ class SendMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Send Mail',
+            subject: config('app.name') . ' - ' . $this->subject,
         );
     }
 
@@ -37,7 +60,13 @@ class SendMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            markdown: 'emails.general-notification',
+            with: [
+                'content' => $this->emailContent,
+                'actionUrl' => $this->actionUrl,
+                'actionText' => $this->actionText,
+                'appName' => config('app.name'),
+            ],
         );
     }
 
