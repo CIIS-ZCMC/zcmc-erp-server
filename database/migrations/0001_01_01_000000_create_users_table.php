3974087e3a0ba86c3c78cb181cef5d277ab5339e
mustaham <mustahammicah@gmail.com>
2025-05-24 09:00:57 +0800

    feat(model & migration): auth pin added in User Model

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('umis_employee_profile_id')->nullable();
+            $table->string('authorization_pin', 10)->nullable();
             $table->string('name');
             $table->string('email')->nullable();
             $table->string('profile_url')->nullable();
@@ -22,20 +23,20 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+//        Schema::create('password_reset_tokens', function (Blueprint $table) {
+//            $table->string('email')->primary();
+//            $table->string('token');
+//            $table->timestamp('created_at')->nullable();
+//        });
+//
+//        Schema::create('sessions', function (Blueprint $table) {
+//            $table->string('id')->primary();
+//            $table->foreignId('user_id')->nullable()->index();
+//            $table->string('ip_address', 45)->nullable();
+//            $table->text('user_agent')->nullable();
+//            $table->longText('payload');
+//            $table->integer('last_activity')->index();
+//        });
     }
 
     /**
