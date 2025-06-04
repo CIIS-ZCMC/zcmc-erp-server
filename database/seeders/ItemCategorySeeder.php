3b211b4f58241465adc292efb594031626606c1a
TjayAmit <tristanjayamit0813@gmail.com>
2025-06-04 09:15:26 +0800

    basta update

--- database/seeders/ItemCategorySeeder.php
+++ database/seeders/ItemCategorySeeder.php
@@ -18,74 +18,43 @@ class ItemCategorySeeder extends Seeder
 
         // Main categories to be seeded
         $mainCategories = [
-            ['name' => 'Software and Subscription', 'code' => 'SAS', 'reference_terminology_id' => 1],
-            ['name' => 'Equipment', 'code' => 'EQP', 'reference_terminology_id' => 1],
-            ['name' => 'Services and Consultancy', 'code' => 'SAC', 'reference_terminology_id' => 1],
-            ['name' => 'Training Expenses', 'code' => 'TRE', 'reference_terminology_id' => 1],
-            ['name' => 'Furniture and Fixture', 'code' => 'FAF', 'reference_terminology_id' => 1],
-            ['name' => 'Supply', 'code' => 'SUP', 'reference_terminology_id' => 1],
-            ['name' => 'Instrument', 'code' => 'INS', 'reference_terminology_id' => 1],
-            ['name' => 'Book, Journal, Publication', 'code' => 'BJP', 'reference_terminology_id' => 1],
-            ['name' => 'Maintenance', 'code' => 'MNT', 'reference_terminology_id' => 1],
-            ['name' => 'Parts and Spare', 'code' => 'PAS', 'reference_terminology_id' => 1],
-            ['name' => 'Tax, License, Membership/Accreditation/Registration', 'code' => 'TLM', 'reference_terminology_id' => 1],
-            ['name' => 'Subscription, Postage & Insurance', 'code' => 'SPI', 'reference_terminology_id' => 1],
-            ['name' => 'Building and Construction', 'code' => 'BAC', 'reference_terminology_id' => 1],
-            ['name' => 'Celebration and Events', 'code' => 'CAE', 'reference_terminology_id' => 1],
-            ['name' => 'Instruments and Tools', 'code' => 'IAT', 'reference_terminology_id' => 1],
-            ['name' => 'Implant', 'code' => 'IMP', 'reference_terminology_id' => 1],
+            ['name' => 'Book, Journal, Publication', 'code' => 'BJP'],
+            ['name' => 'Building and Construction', 'code' => 'BAC'],
+            ['name' => 'Celebration and Events', 'code' => 'CAE'],
+            ['name' => 'Construction Supply', 'code' => 'COS'],
+            ['name' => 'Drugs and Medicine', 'code' => 'DAM'],
+            ['name' => 'Food Supply', 'code' => 'FOS'],
+            ['name' => 'Furniture and Fixture', 'code' => 'FAF'],
+            ['name' => 'ICT Equipment', 'code' => 'ICT'],
+            ['name' => 'ICT Maintenance', 'code' => 'ICM'],
+            ['name' => 'Implant', 'code' => 'IMP'],
+            ['name' => 'Janitorial Supply', 'code' => 'JAS'],
+            ['name' => 'Kitchen Equipment', 'code' => 'KIE'],
+            ['name' => 'Laboratory Supply', 'code' => 'LAS'],
+            ['name' => 'Linen and Laundry Supply', 'code' => 'LLS'],
+            ['name' => 'Medical Equipment', 'code' => 'MEE'],
+            ['name' => 'Medical Equipment Maintenance', 'code' => 'MEM'],
+            ['name' => 'Medical Instrument', 'code' => 'MIN'],
+            ['name' => 'Medical Supply', 'code' => 'MES'],
+            ['name' => 'Office Supply', 'code' => 'OFS'],
+            ['name' => 'Other Equipment', 'code' => 'OTE'],
+            ['name' => 'Other Equipment Maintenance', 'code' => 'OEM'],
+            ['name' => 'Other Fees, Subscription, Processing', 'code' => 'OFS'],
+            ['name' => 'Other Supply', 'code' => 'OTS'],
+            ['name' => 'Other tools', 'code' => 'OTT'],
+            ['name' => 'Parts and Spare', 'code' => 'PAS'],
+            ['name' => 'Services and Consultancy', 'code' => 'SAC'],
+            ['name' => 'Software and Subscription', 'code' => 'SAS'],
+            ['name' => 'Training Expenses', 'code' => 'TRE'],
         ];
 
-        // First create all main categories
-        $categoryMap = [];
         foreach ($mainCategories as $category) {
-            $newCategory = ItemCategory::create([
+             ItemCategory::create([
                 'name' => $category['name'],
                 'code' => $category['code'],
-                'description' => 'Main category for ' . $category['name'],
-                'reference_terminology_id' => $category['reference_terminology_id']
+                'description' => 'Main category for ' . $category['name']
             ]);
 
-            $categoryMap[$category['name']] = $newCategory->id;
-        }
-
-        // Sub-categories with their parent categories
-        $subCategories = [
-            // Equipment sub-categories
-            ['parent' => 'Equipment', 'name' => 'ICT', 'code' => 'EQP-ICT', 'reference_terminology_id' => 1],
-            ['parent' => 'Equipment', 'name' => 'Medical', 'code' => 'EQP-MED', 'reference_terminology_id' => 1],
-            ['parent' => 'Equipment', 'name' => 'Other', 'code' => 'EQP-OTH', 'reference_terminology_id' => 1],
-            ['parent' => 'Equipment', 'name' => 'Kitchen', 'code' => 'EQP-KIT', 'reference_terminology_id' => 1],
-            
-            // Supply sub-categories
-            ['parent' => 'Supply', 'name' => 'Office', 'code' => 'SUP-OFF', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Medical', 'code' => 'SUP-MED', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Food', 'code' => 'SUP-FOD', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Construction', 'code' => 'SUP-CON', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Others', 'code' => 'SUP-OTH', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Janitorial', 'code' => 'SUP-JAN', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Equipment Maintenance', 'code' => 'SUP-EQM', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Linen and Laundry', 'code' => 'SUP-LAL', 'reference_terminology_id' => 1],
-            ['parent' => 'Supply', 'name' => 'Laboratory', 'code' => 'SUP-LAB', 'reference_terminology_id' => 1],
-            
-            // Instrument sub-categories
-            ['parent' => 'Instrument', 'name' => 'Medical', 'code' => 'INS-MED', 'reference_terminology_id' => 1],
-            
-            // Maintenance sub-categories
-            ['parent' => 'Maintenance', 'name' => 'Medical Equipment', 'code' => 'MNT-MED', 'reference_terminology_id' => 1],
-            ['parent' => 'Maintenance', 'name' => 'Other Equipment', 'code' => 'MNT-OEQ', 'reference_terminology_id' => 1],
-            ['parent' => 'Maintenance', 'name' => 'ICT', 'code' => 'MNT-ICT', 'reference_terminology_id' => 1],
-        ];
-
-        // Create all sub-categories
-        foreach ($subCategories as $subCategory) {
-            ItemCategory::create([
-                'item_category_id' => $categoryMap[$subCategory['parent']],
-                'name' => $subCategory['name'],
-                'code' => $subCategory['code'],
-                'description' => 'Sub-category of ' . $subCategory['parent'],
-                'reference_terminology_id' => $subCategory['reference_terminology_id']
-            ]);
         }
     }
 }
