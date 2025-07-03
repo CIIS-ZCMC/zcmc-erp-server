6ad4df1d3b461604b44d2bef82e15c269a0101c9
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2025-07-03 13:49:25 +0800

    pin

--- app/Http/Controllers/AopApplicationController.php
+++ app/Http/Controllers/AopApplicationController.php
@@ -2844,7 +2844,7 @@ class AopApplicationController extends Controller
         $curr_user = User::find($request->user()->id);
         $curr_user_authorization_pin = $curr_user->authorization_pin;
 
-        if ($curr_user_authorization_pin !== $request->authorization_pin) {
+        if ($curr_user_authorization_pin !== $request->pin) {
             return response()->json([
                 'message' => 'Invalid Authorization Pin'
             ], Response::HTTP_BAD_REQUEST);
