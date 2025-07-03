d99c0806a837b1f7c444c15030abdc3e44c1f3a5
= <=>
2025-07-03 13:26:49 +0800

    fix auth pin aop application

--- app/Http/Controllers/AopApplicationController.php
+++ app/Http/Controllers/AopApplicationController.php
@@ -2790,20 +2790,20 @@ class AopApplicationController extends Controller
             return response()->json(['message' => 'Invalid Authorization Pin'], 403);
         }
 
-        // // $activity = $resource->activity;
-        // // $objective = $activity->applicationObjective;
-        // // $aopApplication = $objective->aopApplication ?? null;
-        // // $ppmpApplication = $aopApplication?->ppmpApplication;
+        $activity = $resource->activity;
+        $objective = $activity->applicationObjective;
+        $aopApplication = $objective->aopApplication ?? null;
+        $ppmpApplication = $aopApplication?->ppmpApplication;
 
-        // // if ($ppmpApplication) {
-        // //     \App\Models\PpmpItem::where('ppmp_application_id', $ppmpApplication->id)
-        // //         ->where('item_id', $resource->item_id)
-        // //         ->delete(); // If your model uses SoftDeletes, this will soft-delete
-        // // }
+        if ($ppmpApplication) {
+            \App\Models\PpmpItem::where('ppmp_application_id', $ppmpApplication->id)
+                ->where('item_id', $resource->item_id)
+                ->delete(); // If your model uses SoftDeletes, this will soft-delete
+        }
 
-        // // $resource->delete(); // This assumes Resource model uses SoftDeletes
+        $resource->delete(); // This assumes Resource model uses SoftDeletes
 
-        // return response()->json(['message' => 'Resource successfully deleted.'], 200);
+        return response()->json(['message' => 'Resource successfully deleted.'], 200);
     }
 
     public function destroyActivities(Request $request, Activity $activity)
@@ -2845,7 +2845,7 @@ class AopApplicationController extends Controller
         if ($user->authorization_pin !== $request->authorization_pin) {
             return response()->json(['message' => 'Invalid Authorization Pin'], 403);
         }
-        return response()->json(['message' => 'Authorization Pin is valid.'], 200);
+        return response()->json(['message' => 'Successfully Deleted.'], 200);
     }
 
 
@@ -2886,6 +2886,7 @@ class AopApplicationController extends Controller
                 return null;
         }
     }
+    
     private function getExistingAop($area)
     {
         return AopApplication::where('sector', $area['sector'])
